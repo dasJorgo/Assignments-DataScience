46da2300d76e7885aec4a9daba14ae2cc6b73c99
dasJorgo <g.digkas@gmx.net>
2026-06-17 22:46:18 +0200

    Assignment 1
    Data Science Track Assignment 1
    
    I dont know if I was the problem, but i almost commited the venv bc the folder had no "." but the gitignore had one. could be i didnt set the venv up correctly. so in addition to the assignment i removed the "." in the gitignore

--- src/assignment_1.py
+++ src/assignment_1.py
@@ -3,19 +3,24 @@ import numpy as np
 
 def load_data(file_path: str) -> pd.DataFrame:
     """
-    Load the CSV file using pandas.
+    #Load the CSV file using pandas.
     Return the loaded DataFrame.
     """
+    
+    return pd.read_csv(file_path)
     # TODO: Implement this function
-    pass
+    
 
 def clean_age(df: pd.DataFrame) -> pd.DataFrame:
     """
     Handle missing values in the 'Age' column by filling them with the median age.
     Should return the modified DataFrame.
     """
+    median  = df["Age"].median()
+    df["Age"] = df["Age"].fillna(median)
+    
     # TODO: Implement this function
-    pass
+    return df
 
 def convert_types(df:pd.DataFrame) -> pd.DataFrame:
     """
@@ -23,13 +28,24 @@ def convert_types(df:pd.DataFrame) -> pd.DataFrame:
     Missing values in these columns should be treated as False before conversion.
     Should return the modified DataFrame.
     """
+    #fill false
+    df["VIP"] = df["VIP"].fillna("False")
+    df["CryoSleep"] = df["CryoSleep"].fillna("False")
+    #convert
+    df["VIP"] = df["VIP"].map({"True":True,"False":False}).astype(bool)
+    df["CryoSleep"] = df["CryoSleep"].map({"True":True,"False":False}).astype(bool)
+    
+    
+    # print(df["VIP"].isnull().sum())
+
     # TODO: Implement this function
-    pass
+    return df
 
 if __name__ == "__main__":
     # This block allows students to test their code locally
     try:
         data = load_data("data/train.csv")
+
         data = clean_age(data)
         data = convert_types(data)
         print("Basic cleaning successful!")
